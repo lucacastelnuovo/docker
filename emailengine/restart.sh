5dad79f8d8f539e3ee0fa5b27506d448a9f934a4
Luca Castelnuovo <luca@castelnuovo.xyz>
2022-07-14 17:53:44 +0200

    Update restart script

--- emailengine/restart.sh
+++ emailengine/restart.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
-try() { "$@" || die "cannot $*"; }
-
-set -o allexport
-source .env set
-+o allexport
-
-# Ask user questions and init script
-read -p "SUDO password: " sudoPassword
-read -p "Email account password: " emailPassword
-
 # Stop container
 cd /home/luca/emailengine/
 docker-compose down
 
 # Delete redis cache
-echo $sudoPassword | sudo -S -k rm redis/dump.rdb
+sudo rm redis/dump.rdb
 
-# Restart container
+# Start container
 docker-compose up -d
 
 # Wait 20 seconds for container startup
 sleep 10
 
+# Inform user
+echo "Emailengine restarted"
+echo "https://email.castelnuovo.xyz"
+echo \n
+echo "1. Start free trial"
+echo "2. Enable TLS & save settings"
+echo "3. Create access token"
+
+# Ask user questions and init script
+echo \n
+read -p "Access token: " accessToken
+read -p "Email password: " emailPassword
+
 # Create email account
 curl -XPOST "https://email.castelnuovo.xyz/v1/account" \
-    -H "Authorization: Bearer " + $EENGINE_PREPARED_TOKEN \
+    -H "Authorization: Bearer " + $accessToken \
     -H "Content-type: application/json" \
     -d '{
       "account": "example",
@@ -46,3 +47,8 @@ curl -XPOST "https://email.castelnuovo.xyz/v1/account" \
         "secure": true
       }
     }'
+
+# Create and update tokens
+echo \n
+echo "Create SMTP tokens for:"
+echo "1. ldap.castelnuovo.xyz"
